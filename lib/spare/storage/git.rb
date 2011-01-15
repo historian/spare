30da5297a350dc5d7ee75a578dd3f0f506a0ad3c
Simon Menke <simon.menke@gmail.com>
2011-01-15 17:33:30 +0100

    Better restore

--- lib/spare/storage/git.rb
+++ lib/spare/storage/git.rb
@@ -1,147 +1,213 @@
 class Spare::Storage::Git
-  
+
+  require 'shellwords'
+
   Spare::Storage.register_adapter(:git, self)
-  
+
   def initialize(config)
     @config = config
   end
-  
+
   def backup
-    storage_config = @config.storage_config
+    storage_config       = @config.storage_config
     ENV['GIT_DIR']       = File.expand_path(storage_config[:repository])
     ENV['GIT_WORK_TREE'] = File.expand_path(".")
-    
+
     # Ensure backup repo
     unless File.directory?(ENV['GIT_DIR'])
-      out = `git init . 2>&1`
-      unless $?.exitstatus == 0
-        out = out.gsub("\n", "\n              ")
-        puts "  *** FAILED: #{out}"
-        return false
-      end
+      return unless git(:init, '.')
     end
-    
+
     files = []
     @config.backup_tasks.each do |_, task|
       files.concat task.resolve_files
     end
     files = files.uniq.sort
-    
+
     changes = []
-    
+
     # Find changed files
-    out = `git status --porcelain 2>&1`
-    unless $?.exitstatus == 0
-      out = out.gsub("\n", "\n              ")
-      puts "  *** FAILED: #{out}"
-      out = ""
-    end
-    
-    out.split("\n").each do |line|
-      line = line.strip
-      status, path = line[0,1], line[2..-1].strip
-      path, old_path = *path.split(/\s+\-\>\s+/, 2).reverse
-      
-      case status
-      when ' '
-        # ignore
-      when 'R'
-        changes << ['A', path] if files.include?(path)
-        changes << ['D', old_path]
-      when 'M', 'A', '?', 'C'
-        status = 'A' if status == 'C' or status == '?'
-        changes << [status, path] if files.include?(path)
-      when 'D'
-        changes << ['D', path]
+    git(:status, '--porcelain') do |s, out|
+      if s == 0
+        out.split("\n").each do |line|
+          line = line.strip
+          status, path = line[0,1], line[2..-1].strip
+          path, old_path = *path.split(/\s+\-\>\s+/, 2).reverse
+
+          case status
+          when ' '
+            # ignore
+          when 'R'
+            changes << ['A', path] if files.include?(path)
+            changes << ['D', old_path]
+          when 'M', 'A', '?', 'C'
+            status = 'A' if status == 'C' or status == '?'
+            changes << [status, path] if files.include?(path)
+          when 'D'
+            changes << ['D', path]
+          end
+        end
+      else
+        show_error(out)
       end
     end
-    
-    out = `git ls-tree --full-tree -r --name-only master 2>&1`
-    unless $?.exitstatus == 0
-      unless out =~ /Not a valid object name master/
-        out = out.gsub("\n", "\n              ")
-        puts "  *** FAILED: #{out}"
-      end
-      out = ""
-    end
-    
-    out.split("\n").each do |line|
-      path = line.strip
-      unless files.include?(path)
-        changes << ['D', path]
+
+    git('ls-tree', '--full-tree', '-r', '--name-only', 'master') do |s, out|
+      if s == 0
+        out.split("\n").each do |line|
+          path = line.strip
+          unless files.include?(path)
+            changes << ['D', path]
+          end
+        end
+      elsif out !~ /Not a valid object name master/
+        show_error(out)
       end
     end
-    
+
     if changes.empty?
       puts "No changes since last backup."
       return
     end
-    
+
     changes = changes.uniq
-    
+
     # Stage deleted/updated/new files
     changes.each do |(status, path)|
       case status
       when 'M', 'A'
         puts "#{status} #{path}"
-        out = `git add -f #{path.inspect} 2>&1`
-        unless $?.exitstatus == 0
-          out = out.gsub("\n", "\n              ")
-          puts "  *** FAILED: #{out}"
-        end
-        
+        with_indent(1) { git(:add, '-f', path) }
+
       when 'D'
         puts "D #{path}"
-        out = `git rm --cached #{path.inspect} 2>&1`
-        unless $?.exitstatus == 0
-          out = out.gsub("\n", "\n              ")
-          puts "  *** FAILED: #{out}"
-        end
+        with_indent(1) { git(:rm, '--cached', path) }
 
       end
     end
-    
+
     # Commit changes
     timestamp = Time.now.strftime("Backup: %Y%m%d%H%M%S")
-    out = `git commit -m #{timestamp.inspect} 2>&1`
-    unless $?.exitstatus == 0
-      out = out.gsub("\n", "\n              ")
-      puts "  *** FAILED: #{out}"
-    end
-    
+    git(:commit, '-m', timestamp)
+
     # Push changes
     unless storage_config[:remote]
       puts "There is no remote storage configured."
       return
     end
-    
-    remote = storage_config[:remote].inspect
-    branch = storage_config[:branch].inspect
-    out = `git push #{remote} master:#{branch} 2>&1`
-    unless $?.exitstatus == 0
-      out = out.gsub("\n", "\n              ")
-      puts "  *** FAILED: #{out}"
+
+    remote = storage_config[:remote]
+    branch = storage_config[:branch]
+    git(:push, remote, "master:#{branch}")
+  end
+
+  def restore(ref)
+    storage_config       = @config.storage_config
+    remote               = storage_config[:remote]
+    branch               = storage_config[:branch]
+    ENV['GIT_DIR']       = File.expand_path(storage_config[:repository])
+    ENV['GIT_WORK_TREE'] = File.expand_path(".")
+    timestamp = Time.now.strftime("%Y%m%d%H%M%S")
+
+    unless File.directory?(ENV['GIT_DIR'])
+      return unless git(:init, '.')
+    end
+
+    rem_ref = git('ls-remote', '--heads', '--tags', remote)
+    if rem_ref
+      rem_ref = rem_ref.split("\n").map do |line|
+        line.split(/\s+/, 2).reverse
+      end.flatten
+
+      rem_ref = Hash[*rem_ref]
+      rem_ref = rem_ref[ref]
+    end
+
+    old_ref = git(:log, '-n', 1, '--format=format:%H', 'master')
+
+    if rem_ref and rem_ref == old_ref
+      puts "Already at #{rem_ref}"
+      return
+    end
+
+    if ref == old_ref
+      puts "Already at #{ref}"
+      return
+    end
+
+    if old_ref
+      message = <<-EOM
+      Restoring a backup (at #{timestamp})
+        remote: #{remote}
+        branch: #{branch}
+       old ref: #{old_ref}
+       new ref: #{ref}
+      EOM
+      message = message.gsub(/^      /m, '').strip
+
+      git(:tag, '-a', '-m', message, timestamp)
+      git(:push, remote, '--tags')
+      git(:push, remote, ":#{branch}")
+    end
+
+    git(:fetch, '--depth=4', remote, ref)
+    git(:reset, '--hard', ref)
+    git(:push, remote, "master:#{branch}")
+  end
+
+private
+
+  def sh(*cmd)
+    cmd = cmd.flatten.compact.map { |i| i.to_s }
+    cmd = Shellwords.join(cmd) + ' 2>&1'
+
+    output = `#{cmd}`
+    status = $?.exitstatus
+
+    if block_given?
+      yield(status, output)
+    elsif status != 0
+      show_error(output)
+      false
+    else
+      output
     end
   end
-  
-  def restore
-    
+
+  def git(*cmd, &block)
+    sh('git', *cmd, &block)
   end
-  
+
+  def show_error(out, indent=nil)
+    indent ||= (@indent || 0)
+
+    out = "*** FAILED:\n#{out}"
+    out = out.gsub("\n", "\n" + ("  " * indent))
+    $stderr.puts out
+    $stderr.flush
+  end
+
+  def with_indent(level)
+    _indent, @indent = @indent = level
+    yield
+  ensure
+    @indent = _indent
+  end
+
   class Configuration
-    
+
     def repository(url)
       @repository = url
     end
-    
+
     def remote(remote)
       @remote = remote
     end
-    
+
     def branch(branch)
       @branch = branch
     end
-    
+
     def to_options
       {
         :repository => (@repository || 'backup.git'),
@@ -149,7 +215,7 @@ class Spare::Storage::Git
         :branch     => (@branch || 'master')
       }
     end
-    
+
   end
-  
+
 end
--- lib/spare/task.rb
+++ lib/spare/task.rb
@@ -1,8 +1,8 @@
 class Spare::Task
-  
+
   def initialize(base_name, &block)
     @base_name = base_name
-    
+
     if Rake.application.current_scope.empty?
       Rake.application.in_namespace 'data' do
         install_master_tasks
@@ -13,85 +13,89 @@ class Spare::Task
       instance_eval(&block)
     end
   end
-  
+
   def before_backup(*args, &block)
     task = Rake::Task.define_task(*args, &block)
     Rake::Task.define_task("before_backup" => task.name)
     task
   end
-  
+
   def backup(&block)
     task = Spare::BackupTask.define_task("#{@base_name}:backup", &block)
     @config.backup_tasks[task.name] = task
     Rake::Task.define_task("checkin_backup" => task.name)
     task
   end
-  
+
   def after_backup(*args, &block)
     task = Rake::Task.define_task(*args, &block)
     Rake::Task.define_task("after_backup" => task.name)
     task
   end
-  
+
   def before_restore(*args, &block)
     task = Rake::Task.define_task(*args, &block)
     Rake::Task.define_task("before_restore" => task.name)
     task
   end
-  
+
   def restore(&block)
     task = Spare::RestoreTask.define_task("#{@base_name}:restore", &block)
     @config.restore_tasks[task.name] = task
     Rake::Task.define_task("checkout_restore" => task.name)
     task
   end
-  
+
   def after_restore(*args, &block)
     task = Rake::Task.define_task(*args, &block)
     Rake::Task.define_task("after_restore" => task.name)
     task
   end
-  
+
 private
-  
+
   def install_master_tasks
     name    = Rake.application.current_scope.join(':')
     @config = Spare.configurations[name]
     unless @config
       raise "No configuration for #{name} tasks"
     end
-    
+
     name = [name, 'backup'].flatten.join(':')
-    
+
     unless Rake::Task.task_defined?(name)
       t = Rake::Task.define_task('backup')
       t.add_description "Make a new backup"
-      
+
       t = Rake::Task.define_task('restore')
       t.add_description "Restore a backup"
-      
+
       Rake::Task.define_task("before_backup")
       Rake::Task.define_task("checkin_backup" => 'before_backup')
       Rake::Task.define_task("real_backup"    => 'checkin_backup')
       Rake::Task.define_task("real_backup"    => 'before_backup')
       Rake::Task.define_task("after_backup"   => 'real_backup')
       Rake::Task.define_task("backup"         => 'after_backup')
-      
+
       Rake::Task.define_task('before_restore')
       Rake::Task.define_task("real_restore"     => 'before_restore')
       Rake::Task.define_task("checkout_restore" => 'real_restore')
       Rake::Task.define_task("after_restore"    => 'checkout_restore')
       Rake::Task.define_task("restore"          => 'after_restore')
-      
+
       Rake::Task.define_task('real_backup') do
         @config.storage.new(@config).backup
       end
-      
-      Rake::Task.define_task('real_restore') do
-        @config.storage.new(@config).restore
+
+      Rake::Task.define_task('real_restore', [:ref]) do |t, args|
+        unless args[:ref]
+          puts "Please provide a REF=<> argument"
+          exit 1
+        end
+        @config.storage.new(@config).restore(args[:ref])
       end
-      
+
     end
   end
-  
+
 end
